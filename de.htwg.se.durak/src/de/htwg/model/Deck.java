fa2dc02d91c989f298d4c77d84d0e646c6432350
koenigsberc <king-fabi@gmx.de>
2015-04-10 12:56:15 +0200

    edited class Deck

--- de.htwg.se.durak/src/de/htwg/model/Deck.java
+++ de.htwg.se.durak/src/de/htwg/model/Deck.java
@@ -1,7 +1,17 @@
 package de.htwg.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by fafreibe on 10.04.2015.
  */
 public class Deck {
+
+    private List<PlayingCard> deck;
+
+    public Deck() {
+        deck = new LinkedList<PlayingCard>();
+    }
+
 }
