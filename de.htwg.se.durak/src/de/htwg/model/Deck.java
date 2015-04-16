48c574f48de2e63371611c68c2dfefc06dd0ce57
jawaigel <jawaigel@htwg-konstanz.de>
2015-04-16 10:59:18 +0200

    create deck and shuffle it

--- de.htwg.se.durak/src/de/htwg/model/Deck.java
+++ de.htwg.se.durak/src/de/htwg/model/Deck.java
@@ -2,6 +2,7 @@ package de.htwg.model;
 
 import java.util.LinkedList;
 import java.util.List;
+import static java.util.Collections.*;
 
 /**
  * Created by fafreibe on 10.04.2015.
@@ -11,7 +12,26 @@ public class Deck {
     private List<PlayingCard> deck;
 
     public Deck() {
-        deck = new LinkedList<PlayingCard>();
+        deck = new LinkedList<>();
+
+        for(PlayingCardValue value: PlayingCardValue.values()){
+            for(PlayingCardColor color: PlayingCardColor.values()){
+                deck.add(new PlayingCard(value, color));
+            }
+        }
+        shuffle(deck);
     }
 
+    public int getDeckSize(){
+        return deck.size();
+    }
+
+    @Override
+    public String toString(){
+        String str = "";
+        for( PlayingCard card: deck){
+            str += card.toString() + "\n";
+        }
+        return str;
+    }
 }
