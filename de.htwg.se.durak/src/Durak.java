1de4d806846fdd204a9405c204483d0bbe9788cb
jawaigel <jawaigel@htwg-konstanz.de>
2015-04-16 11:05:54 +0200

    add main

---
+++ de.htwg.se.durak/src/Durak.java
@@ -0,0 +1,18 @@
+import de.htwg.controller.DurakController;
+import de.htwg.model.Deck;
+import de.htwg.view.TUI;
+
+/**
+ * Created by jawaigel on 16.04.2015.
+ */
+public class Durak {
+
+    public static void main(String args[]){
+        DurakController controller = new DurakController();
+        TUI tui = new TUI(controller);
+
+        Deck deck = new Deck();
+
+        System.out.println(deck.toString());
+    }
+}
