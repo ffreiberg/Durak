84a99a1a8b817594d2abdd19f94b20240ca83f71
jawaigel <jawaigel@htwg-konstanz.de>
2015-05-07 13:38:13 +0200

    BorderLayout Version

--- de.htwg.se.durak/src/de/htwg/view/gui/DurakFrame.java
+++ de.htwg.se.durak/src/de/htwg/view/gui/DurakFrame.java
@@ -1,11 +1,50 @@
 package de.htwg.view.gui;
 
+import de.htwg.controller.DurakController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  * Created by jawaigel on 07.05.2015.
  */
-public class DurakFrame {
+public class DurakFrame extends JFrame implements Observer {
 
-    public DurakFrame() {
-        //fjdsf
+    private static final int DEFAULT_X = 480;
+    private static final int DEFAULT_Y = 320;
+
+    private Container pane;
+    private DurakCardPanel panelComputerPlayer, panelHumanPlayer, panelField;
+
+    private DurakController controller;
+
+    public DurakFrame(DurakController controller) {
+        this.controller = controller;
+        controller.addObserver(this);
+
+        setTitle("Durak");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(DEFAULT_X, DEFAULT_Y);
+        pane = getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        //Add Card Panels
+        panelComputerPlayer = new DurakCardPanel();
+        panelHumanPlayer = new DurakCardPanel();
+        panelField = new DurakCardPanel();
+
+        pane.add(panelComputerPlayer, BorderLayout.NORTH);
+        pane.add(panelField, BorderLayout.CENTER);
+        pane.add(panelHumanPlayer, BorderLayout.SOUTH);
+
+        setVisible(true);
+        repaint();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        repaint();
     }
 }
