c82c8dcde2b0e3daeea5bc61837065b411077d6b
koenigsberc <king-fabi@gmx.de>
2015-05-07 13:14:25 +0200

    edited tests, added method toString() to Field.java

--- de.htwg.se.durak/test/de/htwg/model/FieldTest.java
+++ de.htwg.se.durak/test/de/htwg/model/FieldTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import static org.junit.Assert.*;
 
 public class FieldTest {
@@ -21,27 +24,37 @@ public class FieldTest {
     }
 
     @Test
-    public void testAddCard() throws Exception {
-
-    }
-
-    @Test
     public void testGetCard() throws Exception {
-
+        field.addCard(new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.HEARTS));
+        PlayingCard pc = field.getCard();
+        assertEquals(pc.getColor().ordinal(), new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.HEARTS).getColor().ordinal());
+        assertEquals(pc.getValue().ordinal(), new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.HEARTS).getValue().ordinal());
     }
 
     @Test
     public void testGetField() throws Exception {
+        LinkedList<PlayingCard> expected = new LinkedList<>(Arrays.asList(new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.HEARTS),
+                new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.CLUBS)));
+        field.addCard(new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.HEARTS));
+        field.addCard(new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.CLUBS));
 
+        for (int i = 0; i < expected.size(); ++i) {
+            assertEquals(expected.get(i).getColor().ordinal(), field.getField().get(i).getColor().ordinal());
+            assertEquals(expected.get(i).getValue().ordinal(), field.getField().get(i).getValue().ordinal());
+        }
     }
 
     @Test
     public void testClearField() throws Exception {
-
+        field.addCard(new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.HEARTS));
+        field.clearField();
+        assertEquals(0, field.getFieldSize());
     }
 
     @Test
     public void testGetFieldSize() throws Exception {
-
+        field.addCard(new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.HEARTS));
+        field.addCard(new PlayingCard(PlayingCardValue.SIX, PlayingCardColor.CLUBS));
+        assertEquals(2, field.getFieldSize());
     }
 }
