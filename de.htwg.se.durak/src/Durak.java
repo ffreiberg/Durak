5489ef3c128fb130274e43cae5df85c1c21c314c
koenigsberc <king-fabi@gmx.de>
2015-05-18 20:53:00 +0200

    rules compliance

--- de.htwg.se.durak/src/Durak.java
+++ de.htwg.se.durak/src/Durak.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
  */
 public final class Durak {
 
+    private Durak(){}
+
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
         DurakController controller = new DurakController();
