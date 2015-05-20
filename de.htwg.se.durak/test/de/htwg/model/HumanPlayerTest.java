c6990e447a9b142a8a9272fac4e1b8364124f5e7
koenigsberc <king-fabi@gmx.de>
2015-05-20 12:41:04 +0200

    satisfying jenkins

--- de.htwg.se.durak/test/de/htwg/model/HumanPlayerTest.java
+++ de.htwg.se.durak/test/de/htwg/model/HumanPlayerTest.java
@@ -107,8 +107,20 @@ public class HumanPlayerTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testDefendTryToBeatLowerCard() throws Exception {
-        cardToBeat.setTrump();
         PlayingCard p = player.defend(cardToBeat, 1);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testDefendTryToBeatWrongCard() throws Exception {
+        cardToBeat.setTrump();
+        PlayingCard p = player.defend(cardToBeat, 2);
+    }
+
 }
+
+/*
+    true - true:    hab ich
+    true - false:
+    false - true:   hab ich
+    false - false:
+*/
