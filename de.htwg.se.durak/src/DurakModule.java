42614176742be87821e733216dceed4fb2a90ea0
koenigsberc <king-fabi@gmx.de>
2015-06-10 10:28:22 +0200

    Merge branch 'new_master' of Z:\Software Engineering\Durak with conflicts.

---
+++ de.htwg.se.durak/src/DurakModule.java
@@ -0,0 +1,13 @@
+import com.google.inject.AbstractModule;
+
+/**
+ * Created by fafreibe on 10.06.2015.
+ */
+public class DurakModule extends AbstractModule {
+
+
+    @Override
+    protected void configure() {
+
+    }
+}
